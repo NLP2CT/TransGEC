fa4e3acb33a2d7f27b9474e21ca35bde065ef8df
fangt-gec <66154888+fangtao-123@users.noreply.github.com>
2022-03-21 18:30:39 +0800

    Update transgec-generate.sh

--- transgec-generate.sh
+++ transgec-generate.sh
@@ -31,7 +31,8 @@ python2 $YOUR_PATH/transformers/evaluation_scorer/m2scorer/scripts/m2scorer.py \
         $YOUR_PATH/TransGEC.en.model/gen/rep-tok-generated_predictions.txt \
         $YOUR_PATH/data_en/test/official-2014.combined.m2 \
         | tee  $YOUR_PATH/TransGEC.en.model/gen/result_score.txt
-        
+    
+    
 # German #####################################################################################        
 # --- for TransGEC.de.model generation and evaluation ---
 
@@ -61,14 +62,15 @@ python2 $YOUR_PATH/transformers/evaluation_scorer/m2scorer/scripts/m2scorer.py \
         $YOUR_PATH/data_de/test/fm-test-de.m2 \
         | tee  $YOUR_PATH/TransGEC.de.model/gen/result_score.txt
 
+
 # Russian #####################################################################################        
 # --- for TransGEC.ru.model generation and evaluation ---
 
 python $YOUR_PATH/examples/pytorch/translation/run_maxtokens_translation.py \
     --model_name_or_path $YOUR_PATH/TransGEC.ru.model \
     --do_predict \
-    --source_lang de \
-    --target_lang der \
+    --source_lang ru \
+    --target_lang rur \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --max_tokens_per_batch 1024 \
@@ -81,46 +83,47 @@ python $YOUR_PATH/examples/pytorch/translation/run_maxtokens_translation.py \
     --predict_with_generate
     
 python $YOUR_PATH/script/tokenization/spacy_ru.py \
-       $YOUR_PATH/TransGEC.de.model/gen/generated_predictions.txt \
-       $YOUR_PATH/TransGEC.de.model/gen/tok-generated_predictions.txt
+       $YOUR_PATH/TransGEC.ru.model/gen/generated_predictions.txt \
+       $YOUR_PATH/TransGEC.ru.model/gen/tok-generated_predictions.txt
 
 # ---- M2 scorer for Russian----
 python2 $YOUR_PATH/transformers/evaluation_scorer/m2scorer/scripts/m2scorer.py \
         $YOUR_PATH/TransGEC.ru.model/gen/tok-generated_predictions.txt \
         $YOUR_PATH/data_ru/test/RULEC-GEC.test.M2 \
         | tee  $YOUR_PATH/TransGEC.ru.model/gen/result_score.txt
-        
+
+
 # Chinese ##################################################################################### 
 # --- for TransGEC.zh.model generation and evaluation ---
 
 python $YOUR_PATH/examples/pytorch/translation/run_maxtokens_translation.py \
     --model_name_or_path $YOUR_PATH/TransGEC.zh.model \
     --do_predict \
-    --source_lang en \
-    --target_lang ro \
+    --source_lang zh \
+    --target_lang zhr \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --max_tokens_per_batch 1024 \
     --source_prefix "translate Chinese to Chinese: " \
-    --validation_file $YOUR_PATH/data_en//dev/dev.en.json \ \
-    --test_file  $YOUR_PATH/data_en/test/test.en.json \ \
-    --output_dir $YOUR_PATH/TransGEC.en.model/gen \
+    --validation_file $YOUR_PATH/data_zh//dev/dev.zh.json \ \
+    --test_file  $YOUR_PATH/data_zh/test/test.zh.json \ \
+    --output_dir $YOUR_PATH/TransGEC.zh.model/gen \
     --num_beams=5 \
     --overwrite_output_dir \
     --predict_with_generate
     
-# due to the dictionary does not have some Chinese symbols
-python $YOUR_PATH/script/chat.py \
-       $YOUR_PATH/TransGEC.en.model/gen/tok-generated_predictions.txt \
-       $YOUR_PATH/TransGEC.en.model/gen/rep-tok-generated_predictions.txt
-      
- python $YOUR_PATH/script/tokenization/spacy_en.py \
-       $YOUR_PATH/TransGEC.en.model/gen/generated_predictions.txt \
-       $YOUR_PATH/TransGEC.en.model/gen/tok-generated_predictions.txt
- 
-
-# ---- M2 scorer for CoNLL2014 ----
-python2 $YOUR_PATH/transformers/evaluation_scorer/m2scorer/scripts/m2scorer.py \
-        $YOUR_PATH/TransGEC.en.model/gen/rep-tok-generated_predictions.txt \
-        $YOUR_PATH/data_en/test/official-2014.combined.m2 \
-        | tee  $YOUR_PATH/TransGEC.en.model/gen/result_score.txt       
+# Keep the Chinese symbols uniform
+python $YOUR_PATH/script/tokenization/en2zh_char.py \
+       $YOUR_PATH/TransGEC.zh.model/gen/generated_predictions.txt \
+       $YOUR_PATH/TransGEC.zh.model/gen/char-generated_predictions.txt \    
+    
+# remember download PKUNLP toolkit from: 
+python $YOUR_PATH/script/tokenization/pkunlp/pku_tok.py \
+       $YOUR_PATH/TransGEC.zh.model/gen/char-generated_predictions.txt \
+       $YOUR_PATH/TransGEC.zh.model/gen/tok-generated_predictions.txt
+       
+# ---- M2 scorer ----
+python2 $HUG_CODE/evaluation_scorer/m2scorer/scripts/m2scorer.py \
+        $YOUR_PATH/TransGEC.zh.model/gen/tok-generated_predictions.txt \
+        $YOUR_PATH/data_zh/test/gold-zh-test.m2 \
+        | tee $YOUR_PATH/TransGEC.zh.model/gen/result_score.txt
