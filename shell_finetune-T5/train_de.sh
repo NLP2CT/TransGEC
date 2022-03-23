5aca4d8eaf96a48768f66e32a78490089fd42ffd
fangt-gec <66154888+fangtao-123@users.noreply.github.com>
2022-03-23 10:45:25 +0800

    Update train_de.sh

--- shell_finetune-T5/train_de.sh
+++ shell_finetune-T5/train_de.sh
@@ -10,24 +10,24 @@ OUT=$ROOT/out_dir
 # ---- Train -----
 cd $HUG_CODE
 CUDA_VISIBLE_DEVICES=0,1,2,3 MKL_THREADING_LAYER=GNU python $HUG_CODE/examples/pytorch/translation/run_maxtokens_translation.py \
-    --model_name_or_path t5-large \
+    --model_name_or_path mt5-large \
     --do_train \
     --do_eval \
     --do_predict \
-    --source_lang error \
-    --target_lang corret \
+    --source_lang de \
+    --target_lang der \
     --source_prefix "translate German to German: " \
-    --train_file $DATA/train.json \
-    --validation_file $DATA/dev.json \
-    --test_file $DATA/test14.json \
-    --output_dir $OUT/model-en \
+    --train_file $DATA/train.de.translationese.json \
+    --validation_file $DATA/dev.de.json \
+    --test_file $DATA/test.de.json \
+    --output_dir $OUT/model-de \
     --per_device_train_batch_size 12 \
     --per_device_eval_batch_size 12 \
     --max_tokens_per_batch 1536 \
     --gradient_accumulation_steps 128 \
     --max_source_length 128 \
     --max_target_length 128 \
-    --max_steps 600 \
+    --max_steps 800 \
     --learning_rate 0.0007 \
     --num_beams 5 \
     --adafactor \
