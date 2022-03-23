e5adae43ba15d28b839e4d4818112e5721e23dfe
fangt-gec <66154888+fangtao-123@users.noreply.github.com>
2022-03-23 10:51:24 +0800

    Update Generate_evaluate_ru.sh

--- shell_finetune-T5/Generate_evaluate_ru.sh
+++ shell_finetune-T5/Generate_evaluate_ru.sh
@@ -7,14 +7,14 @@ OUT=$ROOT/out_dir
 python $HUG_CODE/examples/pytorch/translation/run_maxtokens_translation.py \
     --model_name_or_path $OUT/model-ru/checkpoint \
     --do_predict \
-    --source_lang error \
-    --target_lang correct \
+    --source_lang ru \
+    --target_lang rur \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --max_tokens_per_batch 512 \
     --source_prefix "translate Russian to Russian: "  \
-    --validation_file $DATA/dev.json \ \
-    --test_file $DATA/test.json \ \
+    --validation_file $DATA/dev.ru.json \ \
+    --test_file $DATA/test.ru.json \ \
     --output_dir $OUT/model-ru/checkpoint/gen \
     --num_beams=5 \
     --overwrite_output_dir \
