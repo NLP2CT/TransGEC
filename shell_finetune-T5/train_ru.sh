1c4385de07c36c89e806f3bee2ef7dfa83e20167
fangt-gec <66154888+fangtao-123@users.noreply.github.com>
2022-03-23 10:47:53 +0800

    Update train_ru.sh

--- shell_finetune-T5/train_ru.sh
+++ shell_finetune-T5/train_ru.sh
@@ -17,10 +17,10 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 MKL_THREADING_LAYER=GNU python $HUG_CODE/examples/p
     --source_lang error \
     --target_lang corret \
     --source_prefix "translate Russian to Russian: "  \
-    --train_file $DATA/train.json \
-    --validation_file $DATA/dev.json \
-    --test_file $DATA/test14.json \
-    --output_dir $OUT/model-en \
+    --train_file $DATA/train.ru.translationese.json \
+    --validation_file $DATA/dev.ru.json \
+    --test_file $DATA/test.ru.json \
+    --output_dir $OUT/model-ru \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --max_tokens_per_batch 1024 \
@@ -39,35 +39,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 MKL_THREADING_LAYER=GNU python $HUG_CODE/examples/p
     --overwrite_output_dir \
     --predict_with_generate
     
-# ---- generation ----
-python $HUG_CODE/examples/pytorch/translation/run_maxtokens_translation.py \
-    --model_name_or_path $OUT/model-en/checkpoint \
-    --do_predict \
-    --source_lang error \
-    --target_lang correct \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --max_tokens_per_batch 512 \
-    --source_prefix "translate Russian to Russian: "  \
-    --validation_file $DATA/dev.json \ \
-    --test_file $DATA/test.json \ \
-    --output_dir $OUT/model-en/checkpoint/gen \
-    --num_beams=5 \
-    --overwrite_output_dir \
-    --predict_with_generate
-    
-# ---- M2 scorer ----
-python $HUG_CODE/evaluation_scorer/tokenization/spacy_ru.py \
-       $OUT/checkpoint/gen/generated_predictions.txt \
-       $OUT/checkpoint/gen/tok-generated_predictions.txt
-
-python2 $HUG_CODE/evaluation_scorer/m2scorer/scripts/m2scorer.py \
-         $OUT/checkpoint/gen/tok-generated_predictions.txt \
-        $OUT/evaluation_scorer/m2scorer//test-M2/RULEC-GEC.test.M2 \
-        | tee $OUT/checkpoint/gen/result_score.txt
-
-
-  
     
 
     
